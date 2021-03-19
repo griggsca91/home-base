b9a76255fab7bbc4ca5c223f94ab57cb66068d9b
Christian Griggs <christian.griggs@walmart.com>
2021-03-19 09:13:55 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/griggsca91/home-base
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.1 // indirect
+	github.com/gorilla/websocket v1.4.2
+	github.com/nats-io/nats-server/v2 v2.2.0 // indirect
+	github.com/nats-io/nats.go v1.10.1-0.20210228004050-ed743748acac
+)
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/nats-io/nats.go"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+} // use default options
+
+var connections = make([]*websocket.Conn, 0)
+
+func passthrough(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	connections = append(connections, c)
+	//for {
+	//	mt, message, err := c.ReadMessage()
+	//	if err != nil {
+	//		log.Println("read:", err)
+	//		break
+	//	}
+	//	log.Printf("recv: %s", message)
+	//	err = c.WriteMessage(mt, message)
+	//	if err != nil {
+	//		log.Println("write:", err)
+	//		break
+	//	}
+	//}
+}
+
+func main() {
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	// Simple Async Subscriber
+	nc.Subscribe("foo", func(m *nats.Msg) {
+		fmt.Printf("Received a message: %s\n", string(m.Data))
+		for index, conn := range connections {
+			if err := conn.WriteMessage(websocket.TextMessage, m.Data); err != nil {
+				log.Println("write:", err)
+				conn.Close()
+				connections = append(connections[:index], connections[index+1:]...)
+			}
+		}
+	})
+
+	http.HandleFunc("/", passthrough)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
