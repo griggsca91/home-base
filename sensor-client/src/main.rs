60308662fd18c2fe332f76614a09cbd50b5b8528
chris <griggsca91@gmail.com>
2021-03-21 10:00:22 +0000

    update

--- sensor-client/src/main.rs
+++ sensor-client/src/main.rs
@@ -1,8 +1,10 @@
 use nats;
 use serde_json::{json};
 use serde::{Serialize, Deserialize};
-use std::time::{SystemTime};
+use std::time::{SystemTime, Duration, Instant};
 use std::{time, thread};
+use rppal::gpio::{Gpio, Level};
+
 
 #[derive(Serialize, Deserialize, Debug)]
 struct GarageSensorState {
@@ -10,7 +12,53 @@ struct GarageSensorState {
     time: u64
 }
 
-fn post_update(conn: &nats::Connection, distance: f64) {
+const GPIO_DISTANCE_READ_PIN: u8 = 24;
+const GPIO_DISTANCE_TRIGGER_PIN: u8 = 20;
+
+/**
+ * 
+ def calculate_distance_inches(high_level_time_secs):
+ return calculate_distance_cm(high_level_time_secs) / 2.54
+
+ def calculate_distance_cm(high_level_time_secs):
+ return ((high_level_time_secs * 340) / 2) * 100
+ */
+
+fn calculate_distance(milliseconds: u128) -> f64 {
+    ((((milliseconds as f64 / 1_000.0) * 340.0) / 2.0) * 100.0) / 2.54
+}
+
+fn post_update(conn: &nats::Connection) {
+    let gpio = Gpio::new().unwrap();
+
+    let mut trigger_pin = gpio.get(GPIO_DISTANCE_TRIGGER_PIN).unwrap().into_output();
+    let mut read_pin = gpio.get(GPIO_DISTANCE_READ_PIN).unwrap().into_input();
+
+    trigger_pin.set_high();
+    thread::sleep(Duration::from_millis(100));
+    trigger_pin.set_low();
+
+    let start = Instant::now();
+    let mut reading = false;
+
+    println!("Reading input");
+
+    loop {
+        let level = read_pin.read();
+        if level == Level::High && reading == false {
+            reading = true
+        }
+        else if level == Level::Low && reading == true {
+            break
+        }
+    }
+
+    let total_time = start.elapsed().as_millis();
+    
+    println!("total time: {:?}", total_time);
+    println!("distance: {:?}", calculate_distance(total_time));
+    let distance = calculate_distance(total_time);
+
     let payload  = GarageSensorState {
         distance: distance,
         time: SystemTime::now()
@@ -33,8 +81,6 @@ fn main() {
 
         thread::sleep(ten_millis);
         println!("posting update");
-        post_update(&nc, 10.3)
+        post_update(&nc)
     }
-
-    post_update(&nc, 10.3)
 }
